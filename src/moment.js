4dcc6a9fe1fdbcf295b9b63ab4f996f96518bb01
kbambrick <kbambrick@splunk.com>
2024-06-04 18:00:38 -0700

    build

--- src/moment.js
+++
@@ -1,6 +0,0 @@
-
-var monthsParse = [/^jan/i, /^feb/i, /^maart|mrt.?$/i, /^apr/i, /^mei$/i, /^jun[i.]?$/i, /^jul[i.]?$/i, /^aug/i, /^sep/i, /^okt/i, /^nov/i, /^dec/i];
-
-var monthsParseFixed = [/^jan/i, /^feb/i, /^(maart|mrt)$/i, /^apr/i, /^mei$/i, /^jun[i.]?$/i, /^jul[i.]?$/i, /^aug/i, /^sep/i, /^okt/i, /^nov/i, /^dec/i];
-
-
