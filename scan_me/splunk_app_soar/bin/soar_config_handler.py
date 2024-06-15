d748aef7fa3ac38d5e6d4a2c24328aefc6d0fdfe
kbambrick <kbambrick@splunk.com>
2024-06-15 10:57:14 -0700

    back to moment

--- scan_me/splunk_app_soar/bin/soar_config_handler.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2023-2024 Splunk Inc. All Rights Reserved.
-
-import soar_config_rest_handler
-
-class SoarConfigHandler(soar_config_rest_handler.SoarConfigRESTHandler):
-    def __init__(self, command_line, command_arg):
-        super(SoarConfigHandler, self).__init__(command_line, command_arg)
